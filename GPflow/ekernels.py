904f9be3d345b451973ec04a39674e5fa04b53cb
vdutor <dutordoirv@gmail.com>
2017-04-04 12:53:02 +0200

    expectations of SM kernel

--- GPflow/ekernels.py
+++ GPflow/ekernels.py
@@ -12,6 +12,201 @@ from numpy import pi as nppi
 int_type = settings.dtypes.int_type
 float_type = settings.dtypes.float_type
 
+class SM(kernels.SM):
+
+    def eKdiag(self, X, Xcov=None):
+        """
+        Also known as phi_0: <K_{x,x}>_{q(x)}
+        :param X:
+        :return: N
+        """
+        return self.Kdiag(X)
+
+    def eKxz(self, Z, Xmu, Xcov):
+        """
+        Also known as phi_1: <K_{x, Z}>_{q(x)}.
+        :param Z: MxD inducing inputs
+        :param Xmu: X mean (NxD)
+        :param Xcov: NxDxD
+        :return: NxM
+        """
+        N, M = tf.shape(Xmu)[0], tf.shape(Z)[0]
+        P = self.input_dim # equals D
+        Q = self.Q
+
+        lengthscales = tf.square(self.lengthscales)
+        weights = self.weights
+        frequencies = self.frequencies
+
+        Sigma_q = 1./(4 * nppi**2) * tf.matrix_diag(1./lengthscales) # Q P P
+        Sigma_q_det = tf.matrix_determinant(Sigma_q)
+        Sigma_q_inv = (4 * nppi**2) * tf.matrix_diag(lengthscales) # Q P P
+        Sigma_q_inv_ex = _expand_and_tile(Sigma_q_inv, 4, 0, N) # N Q P P
+
+        S_inv = tf.matrix_inverse(Xcov) # N P P
+        S_inv_ex = _expand_and_tile(S_inv, 4, 1, Q) # N Q P P
+
+        A_q = tf.matrix_inverse(S_inv_ex + Sigma_q_inv_ex) # N Q P P
+        tmp = _expand_and_tile(tf.matmul(A_q, S_inv_ex), 5, 1, M) # N M Q P P
+        m = tf.matmul(tmp, _expand_and_tile(tf.expand_dims(_subs(Xmu,Z), -1), 5, 2, Q)) # N M Q P 1
+
+        nu_q = tf.expand_dims(_expand_and_tile(2 * nppi * frequencies, 3, 0, N), -1) # N Q P 1
+        nu_q_T = tf.transpose(nu_q, perm=[0,1,3,2]) # N Q 1 P
+
+        exp = _expand_and_tile(tf.exp(-.5 * tf.matmul(tf.matmul(nu_q_T, A_q), nu_q)), 5, 1, M) # M M Q 1 1
+        cos = tf.cos(tf.matmul(_expand_and_tile(nu_q_T, 5, 1, M), m)) # N M Q 1 1
+        fac = weights * (2 * nppi)**(P/2.) * tf.sqrt(Sigma_q_det) # Q
+        fac = _expand_and_tile(_expand_and_tile(fac, 2, 0, M), 3, 0, N) # N M Q
+
+        # Calculation c: (2 pi)^(-k/2) |det(C)|^(-1/2) exp(-1/2 (x - mu)^T C^{-1} (x - mu))
+        mean_c = tf.expand_dims(_expand_and_tile(_subs(Xmu,Z), 4, 2, Q), -1)  # N M Q P 1
+        mean_c_T = tf.transpose(mean_c, perm=[0,1,2,4,3])
+        tmp2 = _expand_and_tile(Sigma_q, 4, 0, N) + _expand_and_tile(Xcov, 4, 1, Q) # N Q P P
+        var_c = _expand_and_tile(tmp2, 5, 1, M) # N M Q P P
+        c_fac = 1./tf.sqrt(tf.matrix_determinant(var_c)) * (2 * nppi)**(-P/2.) # N M Q
+        exp2 = tf.exp(-.5 * (tf.matmul(mean_c_T, tf.matmul(tf.matrix_inverse(var_c), mean_c)))) # N M Q 1 1
+        c = c_fac * tf.squeeze(exp2, axis=[3,4])
+
+        # Final
+        res = tf.reduce_sum(c * fac * tf.squeeze(exp, axis=[3,4]) * tf.squeeze(cos, axis=[3,4]), axis= 2) # N M
+        return res
+
+    def eKzxKxz(self, Z, Xmu, Xcov):
+        """
+        Also known as Phi_2.
+        :param Z: MxD
+        :param Xmu: X mean (NxD)
+        :param Xcov: X covariance matrices (NxDxD)
+        :return: NxMxM
+        """
+        N, M = tf.shape(Xmu)[0], tf.shape(Z)[0]
+        P = self.input_dim # equals D of the comment
+        Q = self.Q
+
+        lengthscales = tf.square(self.lengthscales)
+        frequencies = 2 * nppi * self.frequencies
+        weights = self.weights
+
+        # C
+        Sigma_q = 1./(4 * nppi**2) * tf.matrix_diag(1./lengthscales) # Q P P
+        Sigma_q_det = tf.matrix_determinant(Sigma_q) # Q
+        Sigma_q_det_cross = _prod(Sigma_q_det, Sigma_q_det) # Q Q
+        weights_cross = _prod(weights, weights) # Q Q
+        C = weights_cross * tf.sqrt(Sigma_q_det_cross) * (2*nppi)**P # Q Q
+        C = expand_and_tile(expand_and_tile(expand_and_tile(C, 3, 0, M), 4, 0, M), 5, 0, N) # N M M Q Q
+
+
+        # c1
+        c1_var = expand_and_tile(expand_and_tile(_sum(Sigma_q, Sigma_q), 5, 0, M), 6, 0, M) # M M Q Q P P
+        tau = tf.expand_dims(expand_and_tile(expand_and_tile(_subs(Z,Z), 4, 2, Q), 5, 2, Q), -1) # M M Q Q P 1
+        tau_T = tf.transpose(tau, perm=[0, 1, 2, 3, 5, 4]) # M M Q Q 1 P
+        c1_fac = 1./tf.sqrt(tf.matrix_determinant(c1_var)) * (2 * nppi)**(-P/2.) # M M Q Q
+        c1_exp = tf.exp(-.5 * (tf.matmul(tau_T, tf.matmul(tf.matrix_inverse(c1_var), tau)))) # M M Q Q 1 1
+        c1 = expand_and_tile(c1_fac * tf.squeeze(c1_exp, axis=[4,5]), 5, 0, N) # N M M Q Q
+
+        # V1
+        Sigma_q_inv = (4 * nppi**2) * tf.matrix_diag(lengthscales) # Q P P
+        V1 = tf.matrix_inverse(_sum(Sigma_q_inv, Sigma_q_inv)) # Q Q P P
+        V1_ext = expand_and_tile(expand_and_tile(V1, 5, 0, M), 6, 0, M) # M M Q Q P P
+
+        # m1
+        Sigma_q_inv_ext = expand_and_tile(Sigma_q_inv, 4, 0, M) # M Q P P
+        y_ext = tf.expand_dims(expand_and_tile(Z, 3, 1, Q), -1) # M Q P 1
+        tmp = tf.matmul(Sigma_q_inv_ext, y_ext) # M Q P 1
+        tmp_ext1 = expand_and_tile(expand_and_tile(tmp, 5, 0, M), 6, 2, Q) # M M Q Q P 1
+        tmp_ext2 = expand_and_tile(expand_and_tile(tmp, 5, 2, Q), 6, 1, M) # M M Q Q P 1
+        m1 = tf.matmul(V1_ext, tmp_ext1 + tmp_ext2) # M M Q Q P 1
+
+        # c2
+        S_ext = expand_and_tile(expand_and_tile(expand_and_tile(expand_and_tile(Xcov, 4, 1, M), 5, 1, M), 6, 3, Q), 7, 3, Q) # N M M Q Q P P
+        c2_var = expand_and_tile(V1_ext, 7, 0, N) + S_ext # N M M Q Q P P
+        m1_ext = expand_and_tile(m1, 7, 0, N) # N M M Q Q P 1
+        mu_ext = tf.expand_dims(expand_and_tile(expand_and_tile(expand_and_tile(expand_and_tile(Xmu, 3, 1, M), 4, 1, M), 5, 3, Q), 6, 3, Q), -1) # N M M Q Q P 1
+        tau = m1_ext - mu_ext
+        tau_T = tf.transpose(tau, perm=[0, 1, 2, 3, 4, 6, 5]) # N M M Q Q 1 P
+        c2_fac = 1./tf.sqrt(tf.matrix_determinant(c2_var)) * (2 * nppi)**(-P/2.) # N M M Q Q
+        c2_exp = tf.exp(-.5 * (tf.matmul(tau_T, tf.matmul(tf.matrix_inverse(c2_var), tau)))) # N M M Q Q 1 1
+        c2 = c2_fac * tf.squeeze(c2_exp, axis=[5,6]) # N M M Q Q
+
+        # V2
+        V1_inv_ext = expand_and_tile(_sum(Sigma_q_inv, Sigma_q_inv), 5, 0, N)  # N Q Q P P
+        S_inv = expand_and_tile(expand_and_tile(tf.matrix_inverse(Xcov), 4, 1, Q), 5, 1, Q)  # N Q Q P P
+        V2 = expand_and_tile(expand_and_tile(tf.matrix_inverse(S_inv + V1_inv_ext), 6, 1, M), 7, 1, M) # N M M Q Q P P
+
+        # m2
+        V1_inv_ext = expand_and_tile(expand_and_tile(_sum(Sigma_q_inv, Sigma_q_inv), 5, 0, M), 6, 0, M)  # M M Q Q P P
+        term1 = expand_and_tile(tf.matmul(V1_inv_ext, m1), 7, 0, N) # N M M Q Q P 1
+        term2 = tf.matmul(tf.matrix_inverse(Xcov),tf.expand_dims(Xmu,-1)) # N P 1
+        term2 = expand_and_tile(expand_and_tile(expand_and_tile(expand_and_tile(term2, 4, 1 , Q), 5, 1, Q), 6, 1, M), 7, 1, M) # N M M Q Q P 1
+        m2 = tf.matmul(V2,term1 + term2) # N M M Q Q P 1
+
+        # mean_alpha (ma)
+        nu_cross = tf.transpose(_sum(tf.expand_dims(frequencies, -1), tf.expand_dims(frequencies, -1)), perm=[0, 1, 3, 2]) # Q Q 1 P
+        nu_cross = expand_and_tile(expand_and_tile(expand_and_tile(nu_cross, 5, 0, M), 6, 0, M), 7, 0, N) # N M M Q Q 1 P
+        tmp = tf.matmul(Z,tf.transpose(frequencies)) # M Q
+        tmp_ext1 = expand_and_tile(expand_and_tile(tmp, 3, 0, M), 4, 2, Q) # M M Q Q
+        tmp_ext2 = expand_and_tile(expand_and_tile(tmp, 3, -1, Q), 4, 1, M) # M M Q Q
+        tmp_cross_min = -tmp_ext1 - tmp_ext2 # M M Q Q
+        ma = tf.squeeze(tf.matmul(nu_cross, m2), axis=[5,6]) + expand_and_tile(tmp_cross_min, 5, 0, N) # N M M Q Q
+
+        # mean_beta (mb)
+        nu_cross_min = tf.transpose(_sum(tf.expand_dims(frequencies, -1), tf.expand_dims(-1. * frequencies, -1)), perm=[0, 1, 3, 2]) # Q Q 1 P
+        nu_cross_min = expand_and_tile(expand_and_tile(expand_and_tile(nu_cross_min, 5, 0, M), 6, 0, M), 7, 0, N) # N M M Q Q 1 P
+        tmp_cross_plus = tmp_ext1 - tmp_ext2 # M M Q Q
+        mb = tf.squeeze(tf.matmul(nu_cross_min, m2), axis=[5,6]) + expand_and_tile(tmp_cross_plus, 5, 0, N) # N M M Q Q
+
+        # sigma alpha (sa)
+        sa = tf.squeeze(tf.matmul(tf.matmul(nu_cross, V2), nu_cross, transpose_b=True), axis=[5, 6]) # N M M Q Q
+
+        # sigma alpha (sa)
+        sb = tf.squeeze(tf.matmul(tf.matmul(nu_cross_min, V2), nu_cross_min, transpose_b=True), axis=[5, 6]) # N M M Q Q
+
+        # Finally,
+        res = .5 *  C * c1 * c2 * (tf.exp(-.5 * sa)*tf.cos(ma) + tf.exp(-.5 * sb)*tf.cos(mb)) # N M M Q Q
+        res = tf.reduce_sum(tf.reduce_sum(tf.reduce_sum(res, axis=4), axis=3), axis=0) # M M
+
+    def _expand_and_tile(self, tensor, rank, axis, multiple):
+        """
+        First expands the tensor in one dimension on the 'axis' position,
+        then tiles the tensor 'multiple' times on 'axis'.
+
+        Params:
+        :tensor: tensor that will be expanded and tiled
+        :rank: rank of the tensor after the operation
+        :axis: axis to expand the tensor
+        :multiple: number of times the tensor will be repeated
+
+        Example:
+            tensor A: with shape [2, 2]
+            rank: tf.rank(A) + 1 = 3
+            axis: 1
+            multiple: 3
+
+            returns: tf.tile(tf.expand_dims(A,axis), [1,multiple,1])
+            The result is now rank 3 with shape [2, 3, 2]
+        """
+        return tf.tile(tf.expand_dims(tensor, axis), \
+                       tf.stack([(multiple if ax == axis else 1) for ax in range(rank) ]))
+
+    def _subs(self, X, X2):
+        N, M = tf.shape(X)[0], tf.shape(X2)[0]
+        X = tf.tile(tf.expand_dims(X, 1), [1, M, 1])
+        X2 = tf.tile(tf.expand_dims(X2, 1), [1, N, 1])
+        X2 = tf.transpose(X2, perm=[1, 0, 2])
+        return tf.subtract(X,X2)
+
+    def _prod(self, X, X2):
+        N, M = tf.shape(X)[0], tf.shape(X2)[0]
+        X = tf.tile(tf.expand_dims(X, 1), [1, M])
+        X2 = tf.tile(tf.expand_dims(X2, 0), [N, 1])
+        return X * X2
+
+    def _sum(self, X, X2):
+        N, M = tf.shape(X)[0], tf.shape(X2)[0]
+        X = tf.tile(tf.expand_dims(X, 1), [1, M, 1, 1])
+        X2 = tf.tile(tf.expand_dims(X2, 0), [N, 1, 1, 1])
+        return X + X2
+
 
 class RBF(kernels.RBF):
     def eKdiag(self, X, Xcov=None):
